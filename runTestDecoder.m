2208d9c7fabf7596f8b267df6b5339fd0e065b0d
JorritMontijn <jorritmontijn@gmail.com>
2023-04-20 17:48:14 +0200

    Update runTestDecoder.m

--- runTestDecoder.m
+++ runTestDecoder.m
@@ -35,55 +35,72 @@ end
 matSpikeCounts = getSpikeCounts(cellSpikeTimes,vecOnsets,vecOffsets);
 
 %% test 1
-vecNeuronsX = 1:(intNeuronNum-1);%:round((intNeuronNum/2));
-vecNeuronsY = find(~ismember(1:intNeuronNum,vecNeuronsX));
-vecTrialTypes = vecTrialTypes(randperm(numel(vecTrialAngles)));
-[vecTrialTypeIdx,vecUniqueTrialTypes,vecCounts,cellSelect,vecRepetition] = val2idx(vecTrialTypes);
-varTypeCV = 2;
-dblLambda = 1;
-tic
-[dblPerformanceCV,vecDecodedIndexCV,matPosteriorProbability,dblMeanErrorDegs,matConfusion,matWeights,matAggActivation,matAggWeights,vecRepetition] ...
-	= doCrossValidatedDecodingLR(matSpikeCounts,vecTrialTypes,varTypeCV,[],dblLambda);
-toc
-figure
-subplot(2,3,1)
-imagesc(matConfusion)
-% test 2
-tic
-[dblPerformanceCV2,vecDecodedIndexCV2,matPosteriorProbability2,dblMeanErrorDegs2,matConfusion2] ...
-	= doCrossValidatedDecoding(matSpikeCounts,vecTrialTypes,varTypeCV,vecCounts,dblLambda);
-toc
-subplot(2,3,2)
-imagesc(matConfusion2)
-
-% test 3
-tic
-[dblPerformanceCV3,vecDecodedIndexCV3,matPosteriorProbabilityCV3,dblMeanErrorDegs3,matConfusion3] ...
-	= doCrossValidatedDecodingML(matSpikeCounts,vecTrialTypes,varTypeCV);
-toc
-subplot(2,3,3)
-imagesc(matConfusion3)
-
-% test 4
-tic
-[dblPerformanceCV4,vecDecodedIndexCV2,matPosteriorProbability2,dblMeanErrorDegs2,matConfusion4] ...
-	= doCrossValidatedDecoding(matSpikeCounts,vecTrialTypes,0,vecCounts,dblLambda);
-toc
-subplot(2,3,4)
-imagesc(matConfusion4)
-
-% test 5
-tic
-[dblPerformanceCV5,vecDecodedIndexCV2,matPosteriorProbability2,dblMeanErrorDegs2,matConfusion5] ...
-	= doCrossValidatedDecoding(matSpikeCounts,vecTrialTypes,1,vecCounts,dblLambda);
-toc
-subplot(2,3,5)
-imagesc(matConfusion5)
-
-% test 6
-tic
-[dblPerformanceCV6,vecDecodedIndexCV2,matPosteriorProbability2,dblMeanErrorDegs2,matConfusion6] ...
-	= doCrossValidatedDecoding(matSpikeCounts,vecTrialTypes,vecRepetition,vecCounts,dblLambda);
-toc
-subplot(2,3,6)
-imagesc(matConfusion6)
+for intRand=0:1;
+	figure;drawnow;
+	if intRand == 0
+		vecTrialTypes = vecTrialAngles;
+	else
+		vecTrialTypes = vecTrialTypes(randperm(numel(vecTrialAngles)));
+	end
+	vecNeuronsX = 1:(intNeuronNum-1);%:round((intNeuronNum/2));
+	vecNeuronsY = find(~ismember(1:intNeuronNum,vecNeuronsX));
+	[vecTrialTypeIdx,vecUniqueTrialTypes,vecCounts,cellSelect,vecRepetition] = val2idx(vecTrialTypes);
+	varTypeCV = 2;
+	dblLambda = 1;
+	
+	%old ML
+	tic
+	[dblPerformanceCV_ML,vecDecodedIndexCV_ML,matPosteriorProbabilityCV_ML,dblMeanErrorDegs_ML,matConfusion_ML] ...
+		= doCrossValidatedDecodingML(matSpikeCounts,vecTrialTypes,varTypeCV);
+	toc
+	subplot(2,3,1)
+	imagesc(matConfusion_ML)
+	title('ML, repetition-wise CV')
+	
+	%old LR
+	tic
+	[dblPerformanceCV_LR,vecDecodedIndexCV_LR,matPosteriorProbability_LR,dblMeanErrorDegs_LR,matConfusion_LR] ...
+		= doCrossValidatedDecodingLR(matSpikeCounts,vecTrialTypes,varTypeCV,[],dblLambda);
+	toc
+	subplot(2,3,2)
+	imagesc(matConfusion_LR)
+	title('LR, repetition-wise CV')
+	
+	
+	%new mvn
+	tic
+	[dblPerformanceCV_Mvn,vecDecodedIndexCV_Mvn,matPosteriorProbability_Mvn,dblMeanErrorDegs_Mvn,matConfusion_Mvn] ...
+		= doCrossValidatedDecoding(matSpikeCounts,vecTrialTypes,varTypeCV,vecCounts,dblLambda);
+	toc
+	subplot(2,3,3)
+	imagesc(matConfusion_Mvn)
+	title('Mvn, repetition-wise CV')
+	
+	%new mvn no CV
+	tic
+	[dblPerformanceCV4,vecDecodedIndexCV4,matPosteriorProbability4,dblMeanErrorDegs4,matConfusion4] ...
+		= doCrossValidatedDecoding(matSpikeCounts,vecTrialTypes,0,vecCounts,dblLambda);
+	toc
+	subplot(2,3,4)
+	imagesc(matConfusion4)
+	title('Mvn, no CV')
+	
+	%new mvn single-trial CV
+	tic
+	[dblPerformanceCV5,vecDecodedIndexCV5,matPosteriorProbability5,dblMeanErrorDegs5,matConfusion5] ...
+		= doCrossValidatedDecoding(matSpikeCounts,vecTrialTypes,1,vecCounts,dblLambda);
+	toc
+	subplot(2,3,5)
+	imagesc(matConfusion5)
+	title('Mvn, trial-wise CV')
+	
+	%new mvn k-fold
+	tic
+	[dblPerformanceCV6,vecDecodedIndexCV6,matPosteriorProbability6,dblMeanErrorDegs6,matConfusion6] ...
+		= doCrossValidatedDecoding(matSpikeCounts,vecTrialTypes,vecRepetition,vecCounts,dblLambda);
+	toc
+	subplot(2,3,6)
+	imagesc(matConfusion6)
+	title('Mvn, K-fold CV')
+	drawnow;
+end
